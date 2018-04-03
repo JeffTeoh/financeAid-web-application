49a88ae049cde7a1bf248188a4a2defe62bb0a5f
JeffTeoh <jeffrey@gutstudio.com>
2018-04-04 06:28:06 +0800

    Create sidebar for each screen

---
+++ financeAid.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    //toggle sidebar when button clicked
+    $('.sidebar-toggle').on('click', function(){
+        $('.sidebar').toggleClass('toggled');
+    });
+});
