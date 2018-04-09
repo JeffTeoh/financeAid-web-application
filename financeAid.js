6db16d34efff37111d6a07f2b750517a6ce37e8d
JeffTeoh <jeffrey@gutstudio.com>
2018-04-09 11:07:08 +0800

    Display average amount for each day to spent

--- financeAid.js
+++ financeAid.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
     $('.sidebar-toggle').on('click', function(){
         $('.sidebar').toggleClass('toggled');
     });
+
+    //toggle sidebar when button clicked
+    $('.sidebar-toggleright').on('click', function(){
+        $('.sidebar-right').toggle(200);
+    });
 });
