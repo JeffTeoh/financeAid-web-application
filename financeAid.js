d1b1b888d75457fce24ff1daade93396674f7c9a
JeffTeoh <jeffrey@gutstudio.com>
2018-04-10 12:15:31 +0800

    Create responsive design for side bars in Main Menu

--- financeAid.js
+++ financeAid.js
@@ -3,14 +3,29 @@ $(document).ready(function(){
     $('.sidebar-toggle').on('click', function(){
         $('.sidebar').toggleClass('toggled');
     });
+
+    $(".sidebar-toggleright").on('click', function(){
+        $(".sidebar-right").toggle();
+    });
+
+    if ($(window).width() < 1200) {
+        $("#sidebar").addClass("toggled");
+        $("#sidebar-right").css("display", "none");
+    }
+    else {
+        $("#sidebar").removeClass("toggled");
+        $("#sidebar-right").css("display", "block");
+    }
     
     //hide side bar while resize
     $(window).resize(function(){
         if ($(this).width() < 1200) {
             $("#sidebar").addClass("toggled");
+            $("#sidebar-right").css("display", "none");
         }
         else {
             $("#sidebar").removeClass("toggled");
+            $("#sidebar-right").css("display", "block");
         }
     });
       
